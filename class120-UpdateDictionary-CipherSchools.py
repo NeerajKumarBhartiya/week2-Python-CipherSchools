192efbc4a808917ae27e95c5f8754f5412d3d8f2
Neeraj Kumar <106676463+NeerajKumarBhartiya@users.noreply.github.com>
2022-12-14 23:28:04 +0530

    Add files via upload

---
+++ class120-UpdateDictionary-CipherSchools.py
@@ -0,0 +1,12 @@
+# update method
+
+user_info={
+    'name':'neeraj',
+    'age': 20,
+    'fav_movies': ['avengers'],
+    'fav_tunes': ['awakening','fairy tale'],
+}
+
+more_info= {'state' : 'bihar', 'hobbies' : ['coding','reading','guiter']}
+user_info.update({})
+print(user_info)
